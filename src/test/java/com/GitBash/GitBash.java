3d96133a8649582f9606ed2d334f3bce73f7802e
nishchithanithin1990@gmail.com <nishchithanithin1990@gmail.com>
2022-07-25 12:41:08 +0530

    1

---
+++ src/test/java/com/GitBash/GitBash.java
@@ -0,0 +1,12 @@
+package com.GitBash;
+
+public class GitBash {
+
+	public static void main(String[] args) {
+		System.out.println("Git Bash commands");
+		
+		System.out.println("Git Bash change in green colour");
+		//hi good morning
+	}
+
+}
